33254e66c34a4df6595b074b140a866e978d5888
tonghs <tonghuashuai@gmail.com>
2021-06-01 15:51:07 +0800

    fix

--- src/com/tonghs/java/Main.java
+++ src/com/tonghs/java/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        long start = System.currentTimeMillis();
+        for(int i = 0; i < 10000; i++) {
+            System.out.println(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("共耗时：" + (end - start) + "毫秒");
     }
 }
